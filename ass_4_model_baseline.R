24ecb4789ef9169f08c3fdaa5398e77980e8c8d3
pat-k-byrne <pbyrne@ucsb.edu>
2019-12-05 13:13:52 -0800

    Yeah I finished

--- ass_4_model_baseline.R
+++ ass_4_model_baseline.R
@@ -13,23 +13,23 @@ flood.prob.low <- c()
 flood.prob.high <- c()
 
 # Initializing variables
-## Counter variables for number of anomalies greater than 5m
+## Counter variables for number of anomalies greater than 5m aka the number of floods in 10000
 n.low <- 0
 n.high <- 0
-## Store the results of a single anomaly calculation
+## Store the results of a single anomaly calculation/check
 anomaly.low <- 0
 anomaly.high <- 0
-for (i in 1:4) { # For each of the four surge possibilities
-  n.low <- 0 # Set the count back to zero
+for (i in 1:length(surge)) { # For each of the spots in the surge vector aka the four surge possibilities
+  n.low <- 0 # Set the counts back to zero for this i-th cycle through the loop
   n.high <- 0
   for (j in 1:10000){ # For each of the random numbers
     anomaly.low <- sea.lvl.var[j] - 1.25 + surge[i] # Calculate the anomaly for each tide level
     anomaly.high <- sea.lvl.var[j] + 1.25 + surge[i]
-    if (anomaly.low > 5) { # Is the anomaly greater than 5m for low tide?
-      n.low <- n.low + 1 # If so, increase the count by one
+    if (anomaly.low > 5) { # Is the anomaly at this spot in the vector of 10000 random sea levels greater than 5m for low tide?
+      n.low <- n.low + 1 # If so, increase the count of floods by one
     }
-    if (anomaly.high > 5) { # Is the anomaly greater than 5m for high tide?
-      n.high <- n.high + 1 # If so, increase the count by one
+    if (anomaly.high > 5) { # Is the anomaly at this spot in the vector of 10000 random sea levels greater than 5m for high tide?
+      n.high <- n.high + 1 # If so, increase the count of floods by one
     }
   }
   flood.prob.low[i] <- n.low/10000 # Finally, store the risk probability for each of the four surge possibilities
--- ass_4_model_climatechange.R
+++ ass_4_model_climatechange.R
@@ -1,58 +1,232 @@
 # Set the seed so we get the same random numbers every time we run this code
-set.seed(666)
+set.seed(6666)
+library(tidyverse)
 
-# 
-rcp4.5 <- c(0, 2, 7)
-rcp8.5 <- c(0, 11, 31)
-yr <- c(2020, 2025, 2050)
-
-
+# Define our possible mean sea levels at each conservation pathway
+rcp4.5 <- c(0, 2, 7)*0.0254
+rcp8.5 <- c(0, 11, 31)*0.0254
+yr <- c(2000, 2020, 2050)
 
 # Generate a sequence of 10000 random numbers from a normal distribution with a mean of zero and a standard deviation of 3
 # This represents variations in sea level due to climate variability
-sea.lvl.var.4.5 <- rnorm(10000, mean = 0, sd = 3) 
-sea.lvl.var.8.5 <- rnorm(10000, mean = 0, sd = 3) 
-
+sea.lvl.var.4.5 <- c()
+sea.lvl.var.8.5 <- c()
 
 # Create a vector of possible storm surges
 surge <- 0:3
 
-# Create a vector to store the results of our model run
-flood.prob.low.4.5 <- matrix()
-flood.prob.low.8.5 <- matrix()
-flood.prob.high.4.5 <- matrix()
-flood.prob.high.8.5 <- matrix()
+# Create matrices to store the results of our combinations of surge and year for each combination of tide level and pathway
+# Rows are time, columns are surges
+flood.prob.low.4.5 <- matrix(, nrow = length(rcp4.5), ncol = length(surge))
+flood.prob.low.8.5 <- matrix(, nrow = length(rcp4.5), ncol = length(surge))
+flood.prob.mid.4.5 <- matrix(, nrow = length(rcp4.5), ncol = length(surge))
+flood.prob.mid.8.5 <- matrix(, nrow = length(rcp8.5), ncol = length(surge))
+flood.prob.high.4.5 <- matrix(, nrow = length(rcp8.5), ncol = length(surge))
+flood.prob.high.8.5 <- matrix(, nrow = length(rcp8.5), ncol = length(surge))
 
 # Initializing variables
-## Counter variables for number of anomalies greater than 5m
+## Counter variables for number of anomalies greater than 5m for each conservation pathway and tide level
 n.low.4.5 <- 0
+n.mid.4.5 <- 0
 n.high.4.5 <- 0
 n.low.8.5 <- 0
+n.mid.8.5 <- 0
 n.high.8.5 <- 0
-## Store the results of a single anomaly calculation
-anomaly.low <- 0
-anomaly.high <- 0
-anomaly.low <- 0
-anomaly.high <- 0
-for (i in 1:4) { # For each of the four surge possibilities
-  for (j in 1:3) {
-    n.low <- 0 # Set the count back to zero
-    n.high <- 0
+## Store the results of a single anomaly calculation for each conservation pathway and tide level
+anomaly.low.4.5 <- 0
+anomaly.mid.4.5 <- 0
+anomaly.high.4.5 <- 0
+anomaly.low.8.5 <- 0
+anomaly.mid.8.5 <- 0
+anomaly.high.8.5 <- 0
+
+# BROTHER: THE LOOPS 
+for (i in 1:length(surge)) { # For each of the four surge possibilities
+  for (j in 1:length(rcp4.5)) { # And each of the three mean sea levels
+    n.low.4.5 <- 0 
+    n.mid.4.5 <- 0
+    n.high.4.5 <- 0 # Set alllllll the counts back to zero
+    n.low.8.5 <- 0
+    n.mid.8.5 <- 0
+    n.high.8.5 <- 0
+    sea.lvl.var.4.5 <- rnorm(10000, mean = rcp4.5[j], sd = 3) # Find new sea level variations
+    sea.lvl.var.8.5 <- rnorm(10000, mean = rcp8.5[j], sd = 3) 
     for (k in 1:10000){ # For each of the random numbers
-      anomaly.low <- sea.lvl.var[k] - 1.25 + surge[i] # Calculate the anomaly for each tide level
-      anomaly.high <- sea.lvl.var[k] + 1.25 + surge[i]
-      if (anomaly.low > 5) { # Is the anomaly greater than 5m for low tide?
-        n.low <- n.low + 1 # If so, increase the count by one
-      }
-      if (anomaly.high > 5) { # Is the anomaly greater than 5m for high tide?
-        n.high <- n.high + 1 # If so, increase the count by one
-      }
+      anomaly.low.4.5 <- sea.lvl.var.4.5[k] - 1.25 + surge[i] # Calculate the anomaly for each tide level
+      anomaly.mid.4.5 <- sea.lvl.var.4.5[k] + surge[i]
+      anomaly.high.4.5 <- sea.lvl.var.4.5[k] + 1.25 + surge[i]
+      anomaly.low.8.5 <- sea.lvl.var.8.5[k] - 1.25 + surge[i]
+      anomaly.mid.8.5 <- sea.lvl.var.8.5[k] + surge[i]
+      anomaly.high.8.5 <- sea.lvl.var.8.5[k] + 1.25 + surge[i]
+      if (anomaly.low.4.5 > 5){ 
+        n.low.4.5 <- n.low.4.5 + 1} # IF the anomaly is greater than 5m for low tide for rcp4.5, increase the count by 1
+      if (anomaly.mid.4.5 > 5){
+        n.mid.4.5 <- n.mid.4.5 + 1} # Etc for each combo of pathway and tide level
+      if (anomaly.high.4.5 > 5){
+        n.high.4.5 <- n.high.4.5 + 1}
+      if (anomaly.low.8.5 > 5){
+        n.low.8.5 <- n.low.8.5 + 1}
+      if (anomaly.mid.8.5 > 5){
+        n.mid.8.5 <- n.mid.8.5 + 1}
+      if (anomaly.high.8.5 > 5){
+        n.high.8.5 <- n.high.8.5 + 1}
     }
-    flood.prob.low[i] <- n.low/10000 # Finally, store the risk probability for each of the four surge possibilities
-    flood.prob.high[i] <- n.high/10000
+    flood.prob.low.4.5[j,i] <- n.low.4.5/10000
+    flood.prob.mid.4.5[j,i] <- n.mid.4.5/10000
+    flood.prob.high.4.5[j,i] <- n.high.4.5/10000
+    flood.prob.low.8.5[j,i] <- n.low.8.5/10000
+    flood.prob.mid.8.5[j,i] <- n.mid.8.5/10000
+    flood.prob.high.8.5[j,i] <- n.high.8.5/10000
   }
-    
-  
 }
 
+## --------------
+df.0m.tides.4.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.4.5[,1], 
+                              'hightide' = flood.prob.high.4.5[,1])
+df.0m.mid.4.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.4.5[,1])
+## ---
+df.1m.tides.4.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.4.5[,2], 
+                              'hightide' = flood.prob.high.4.5[,2])
+df.1m.mid.4.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.4.5[,2])
+## ---
+df.2m.tides.4.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.4.5[,3], 
+                              'hightide' = flood.prob.high.4.5[,3])
+df.2m.mid.4.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.4.5[,3])
+## ---
+df.3m.tides.4.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.4.5[,4], 
+                              'hightide' = flood.prob.high.4.5[,4])
+df.3m.mid.4.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.4.5[,4])
 
+## --------------
+df.0m.tides.8.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.8.5[,1], 
+                              'hightide' = flood.prob.high.8.5[,1])
+df.0m.mid.8.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.8.5[,1])
+## ---
+df.1m.tides.8.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.8.5[,2], 
+                              'hightide' = flood.prob.high.8.5[,2])
+df.1m.mid.8.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.8.5[,2])
+## ---
+df.2m.tides.8.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.8.5[,3], 
+                              'hightide' = flood.prob.high.8.5[,3])
+df.2m.mid.8.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.8.5[,3])
+## ---
+df.3m.tides.8.5 <- data.frame('year' = yr, 
+                              'lowtide' = flood.prob.low.8.5[,4], 
+                              'hightide' = flood.prob.high.8.5[,4])
+df.3m.mid.8.5 <- data.frame('year' = yr, 
+                            'notide' = flood.prob.mid.8.5[,4])
+
+
+ggplot() +
+  geom_ribbon(data = df.0m.tides.4.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill = 'lightblue1',
+              alpha = 0.4) +
+  geom_line(data = df.0m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'dodgerblue') +
+  geom_point(data = df.0m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'dodgerblue') +
+  geom_ribbon(data = df.1m.tides.4.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill  = 'lightblue2',
+              alpha = 0.4) +
+  geom_line(data = df.1m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'blue') +
+  geom_point(data = df.1m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'blue') +
+  geom_ribbon(data = df.2m.tides.4.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill  = 'lightblue3',
+              alpha = 0.4) +
+  geom_line(data = df.2m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'darkblue') +
+  geom_point(data = df.2m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'darkblue') +
+  geom_ribbon(data = df.3m.tides.4.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill = 'lightblue4',
+              alpha = 0.4) +
+  geom_line(data = df.3m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'midnightblue') + 
+  geom_point(data = df.3m.mid.4.5, 
+            aes(x = year, y = notide),
+            color = 'midnightblue') +
+  labs(
+    x = 'Year',
+    y = 'Flood Probability',
+    title = 'RCP 4.5') +
+  scale_x_continuous(limits = c(2000,2050), 
+                     expand = c(0.01,0.1)) +
+  scale_y_continuous(limits = c(0,0.6),
+                     expand = c(0,0)) 
+
+ggplot() +
+  geom_ribbon(data = df.0m.tides.8.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill = 'lightblue1',
+              alpha = 0.4) +
+  geom_line(data = df.0m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'dodgerblue') +
+  geom_point(data = df.0m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'dodgerblue') +
+  geom_ribbon(data = df.1m.tides.8.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill  = 'lightblue2',
+              alpha = 0.4) +
+  geom_line(data = df.1m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'blue') +
+  geom_point(data = df.1m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'blue') +
+  geom_ribbon(data = df.2m.tides.8.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill  = 'lightblue3',
+              alpha = 0.4) +
+  geom_line(data = df.2m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'darkblue') +
+  geom_point(data = df.2m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'darkblue') +
+  geom_ribbon(data = df.3m.tides.8.5, 
+              aes(x = year, ymin = lowtide, ymax = hightide),
+              fill = 'lightblue4',
+              alpha = 0.4) +
+  geom_line(data = df.3m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'midnightblue') +
+  geom_point(data = df.3m.mid.8.5, 
+            aes(x = year, y = notide),
+            color = 'midnightblue') +
+  labs(
+    x = 'Year',
+    y = 'Flood Probability',
+    title = 'RCP 8.5') +
+  scale_x_continuous(limits = c(2000,2050), 
+                     expand = c(0.01,0.1)) +
+  scale_y_continuous(limits = c(0,0.6),
+                     expand = c(0,0))
